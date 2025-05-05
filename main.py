dda16d64c2095f8bdd7efa9c369f52fbb7297328
Ponlin02 <ponlin.02@gmail.com>
2025-05-05 15:38:09 +0200

    progress
    Connected python with mysql

---
+++ main.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+# Establish the connection
+connection = mysql.connector.connect(
+    host = "localhost",      # Or the IP address of your DB server
+    user = "root",  # The MySQL username (often 'root')
+    password = "",  # The password for that user
+    database = ""  # The name of your schema/database
+)
+
+# Create a cursos to execute queries
+cursor = connection.cursor()
+
+#Example query
+cursor.execute("SELECT * FROM cars")
+
+# Fetch and print results
+for row in cursor.fetchall():
+    print(row)
+
+#clean up
+cursor.close()
+connection.close()
---
+++ queries/test.sql
@@ -0,0 +1,6 @@
+-- Some simple queries to view stuff
+SELECT * FROM matches;
+SELECT * FROM playermatchstats;
+SELECT * FROM players;
+SELECT * FROM teammembers;
+SELECT * FROM teams;
